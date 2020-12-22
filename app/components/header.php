2566d20fd1e26f1f55d5e15ff8c28fb096ac2166
codystack <ebuzzadvert@gmail.com>
2020-12-22 20:11:05 +0100

    registration started

--- app/components/header.php
+++ app/components/header.php
@@ -12,13 +12,17 @@
   <meta property="og:image" content="https://i.imgur.com/XSGaWUp.png"/>
   <meta name="author" content="ThankGod Okoro is a Software developer/programmer, Web + App Developer, UI/UX Designer, Brand Strategist & Product/Graphic Designer.">
   <title>Nigerian Queen&trade; :: Beauty for Purpose</title>
+  
   <!-- Favicon -->
   <link rel="icon" href="https://i.imgur.com/TNznbKa.png" type="image/png">
+
   <!-- Fonts -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
+
   <!-- Icons -->
   <link rel="stylesheet" href="./assets/vendor/nucleo/css/nucleo.css" type="text/css">
   <link rel="stylesheet" href="./assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
+
   <!-- Vote Online CSS -->
   <link rel="stylesheet" href="./assets/css/voteonline.css?v=1.2.0" type="text/css">
   <link rel="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/8.11.8/sweetalert2.min.css">
